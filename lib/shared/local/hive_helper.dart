a9b70c94caef4a4d7a91bb7e9b71bc022fb2f65c
AbdullahElmeslimany <aelmeslimany24@gmail.com>
2024-05-27 17:07:05 +0300

    Shared Local And Network

--- lib/shared/local/hive_helper.dart
+++ lib/shared/local/hive_helper.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -19,5 +21,17 @@ class HiveHelper {
   Future<Map<String, dynamic>> fetchData({required String key}) async {
     final data = await myBox!.get(key);
     return data;
+  }
+
+  Future deleteData({required String key}) async {
+    await myBox!.delete(key);
+  }
+
+  Future deleteAllData() async {
+    await myBox!.clear();
+  }
+
+  Future closeBox() async {
+    await myBox!.close();
   }
 }
